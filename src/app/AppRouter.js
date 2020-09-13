a483e65454a08de81286fe623f23bc107a72a528
Imran Khan <imransilvake@gmail.com>
2020-09-13 09:32:11 +0200

    update parser

--- src/app/AppRouter.js
+++ src/app/AppRouter.js
@@ -6,13 +6,8 @@ import { Switch, Route } from 'react-router-dom';
 import ENV from '../environment/index';
 
 // lazy load components
+const RSSParser = lazy(() => import('./screens/rss-parser/RSS-Parser'));
 const Error404 = lazy(() => import('./screens/404/Error404'));
-
-const RSSParser = lazy(() => {
-	return new Promise(resolve => {
-	  setTimeout(() => resolve(import('./screens/rss-parser/RSS-Parser')), 300000);
-	});
-  });
 
 /**
  * Routing of the app
